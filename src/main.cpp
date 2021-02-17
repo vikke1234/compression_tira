fa10c86b5fc6f762e4de1021155d131ea2d4cb9e
Viktor Horsmanheimo <viktor.horsmanheimo@gmail.com>
2021-02-17 21:43:53 +0200

    added actual interfacing with the program

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <unistd.h>
 
+#include "../headers/heap.h"
+#include "../headers/huffman.h"
 int main(int argc, char *argv[]) {
-
+  std::string help = std::string("Usage: ") + argv[0] +
+                     "\n-d filename \tdecompression\n-c filename \tcompression\n";
+  int opt = 0;
+  if(argc < 2) {
+    std::cerr << help;
+  }
+  while ((opt = getopt(argc, argv, "c:d:")) != -1) {
+    switch (opt) {
+    case 'c':
+      huffman_compression(optarg);
+      break;
+    case 'd':
+      huffman_decompress(optarg);
+      break;
+    default:
+      std::cerr << help;
+    }
+  }
+  return 0;
 }
